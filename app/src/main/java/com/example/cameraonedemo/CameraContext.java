8f9cf34f3723354131bd84ce902242c50f2701be
alaiye <alaiye_work@163.com>
2020-07-05 11:15:32 +0800

    refactor camera context

---
+++ app/src/main/java/com/example/cameraonedemo/CameraContext.java
@@ -0,0 +1,215 @@
+package com.example.cameraonedemo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.OrientationEventListener;
+import android.view.SurfaceHolder;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CameraContext {
+
+    private static final String TAG = "CameraContext";
+
+    private Context context;
+    private Camera camera;
+    private Camera.Parameters parameters;
+    private CameraInfo currCameraInfo;
+    private MyOrientationEventListener orientationEventListener;
+    private SurfaceHolder surfaceHolder;
+
+    private int displayOrientation;
+    private int rotation;
+
+    private Camera.AutoFocusMoveCallback cafCallback = new Camera.AutoFocusMoveCallback() {
+        @Override
+        public void onAutoFocusMoving(boolean start, Camera camera) {
+            Log.e(TAG, "onAutoFocusMoving: start " + start);
+            Toast.makeText(context,
+                    "focus " + (start ? "start" : "end"), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    private Camera.AutoFocusCallback afCallback = new Camera.AutoFocusCallback() {
+        @Override
+        public void onAutoFocus(boolean success, Camera camera) {
+            Log.d(TAG, "onAutoFocus: success = " + success);
+            Toast.makeText(context,
+                    "focus " + (success ? "success" : "fail"), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public CameraContext(Context context) {
+        this.context = context;
+        orientationEventListener = new MyOrientationEventListener(context);
+    }
+
+    public void configSurfaceHolder(SurfaceHolder holder) {
+        surfaceHolder = holder;
+    }
+
+    public void resume() {
+        orientationEventListener.enable();
+    }
+
+    public void pause() {
+        orientationEventListener.disable();
+    }
+
+
+    public void openCamera(int type) {
+        currCameraInfo = new CameraInfo(type);
+        int cameraId = currCameraInfo.getCameraId();
+        camera = Camera.open(cameraId);
+        parameters = camera.getParameters();
+        List<Camera.Size> previewSizeList = parameters.getSupportedPreviewSizes();
+        for (Camera.Size size: previewSizeList) {
+            Log.d(TAG, "preview size w " + size.width + ", h = " + size.height);
+        }
+
+        List<Camera.Size> pictureSizeList = parameters.getSupportedPictureSizes();
+        for (Camera.Size size: pictureSizeList) {
+            Log.d(TAG, "picture size w " + size.width + ", h = " + size.height);
+        }
+        parameters.setPreviewSize(1920, 1080);
+        parameters.setPictureSize(800, 600);
+
+        if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            camera.setAutoFocusMoveCallback(cafCallback);
+        }
+        camera.setParameters(parameters);
+
+        displayOrientation = 0;
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            displayOrientation = CameraUtils.getCameraDisplayOrientation((Activity) context, cameraId);
+            camera.setDisplayOrientation(displayOrientation);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        camera.startPreview();
+        Log.d(TAG, "openCamera: " + currCameraInfo);
+    }
+
+    public void capture(final PictureCallback callback) {
+        if (camera != null) {
+            parameters.setRotation(rotation);
+            camera.setParameters(parameters);
+            camera.takePicture(new Camera.ShutterCallback() {
+                /**
+                 * Called as near as possible to the moment when a photo is captured
+                 * from the sensor.  This is a good opportunity to play a shutter sound
+                 * or give other feedback of camera operation.  This may be some time
+                 * after the photo was triggered, but some time before the actual data
+                 * is available.
+                 */
+                @Override
+                public void onShutter() {
+                    Log.d(TAG, "onShutter: ");
+                }
+            }, new Camera.PictureCallback() {
+                @Override
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    // raw data
+                    Log.d(TAG, "onPictureTaken: raw");
+                }
+            }, new Camera.PictureCallback() {
+                @Override
+                public void onPictureTaken(byte[] data, Camera camera) {
+                    camera.startPreview();
+
+                    Log.d(TAG, "onPictureTaken");
+                    if (callback != null) {
+                        callback.onPictureTaken(data, camera);
+                    }
+                }
+            });
+        }
+    }
+
+    public void switchCamera(int type) {
+        closeCamera();
+        openCamera(type);
+    }
+
+
+    public void onTouchAF(int x, int y,
+                          int focusW, int focusH,
+                          int previewW, int previewH,
+                          boolean isMirror) {
+        // meter
+        int maxNumMeteringAreas = parameters.getMaxNumMeteringAreas();
+        if (maxNumMeteringAreas > 0) {
+            Rect tapRect = CameraUtils.calculateTapArea(
+                    focusW, focusH, x, y,
+                    previewW, previewH, displayOrientation, 1.0f, isMirror);
+            List<Camera.Area> meteringAreas = new ArrayList<>();
+            Camera.Area area = new Camera.Area(tapRect, 1000);
+            meteringAreas.add(area);
+            parameters.setMeteringAreas(meteringAreas);
+            camera.setParameters(parameters);
+        }
+
+        // focus
+        if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+            camera.cancelAutoFocus();
+            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            int maxNumFocusAreas = parameters.getMaxNumFocusAreas();
+            if (maxNumFocusAreas > 0) {
+                List<Camera.Area> areas = new ArrayList<>();
+                Rect tapRect = CameraUtils.calculateTapArea(
+                        focusW, focusH, x, y,
+                        previewW, previewH, displayOrientation,1.5f, isMirror);
+                Camera.Area area = new Camera.Area(tapRect, 1000);
+                areas.add(area);
+                parameters.setFocusAreas(areas);
+            }
+            camera.setParameters(parameters);
+            camera.autoFocus(afCallback);
+            camera.setAutoFocusMoveCallback(null);
+        }
+    }
+
+    public void closeCamera() {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }
+
+    private class MyOrientationEventListener extends OrientationEventListener {
+
+        public MyOrientationEventListener(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void onOrientationChanged(int orientation) {
+            if (orientation == ORIENTATION_UNKNOWN) return;
+            orientation = (orientation + 45) / 90 * 90;
+            int degrees;
+            if (currCameraInfo.getFacing() == CameraInfo.CAMERA_FACING_FRONT) {
+                degrees = (currCameraInfo.getPictureNeedRotateOrientation() - orientation + 360) % 360;
+            } else { // back-facing camera
+                degrees = (currCameraInfo.getPictureNeedRotateOrientation() + orientation) % 360;
+            }
+
+            if (rotation != degrees) {
+                rotation = degrees;
+                Log.d(TAG, "onOrientationChanged: rotation = " + rotation + ", orientation = " + orientation);
+            }
+        }
+    }
+
+    public interface PictureCallback {
+        void onPictureTaken(byte[] data, Camera camera);
+    }
+}
--- app/src/main/java/com/example/cameraonedemo/MainActivity.java
+++ app/src/main/java/com/example/cameraonedemo/MainActivity.java
@@ -5,27 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.pm.PackageManager;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Rect;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.OrientationEventListener;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity
         implements SurfaceHolder.Callback, View.OnClickListener {
@@ -33,39 +28,12 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MainActivity";
 
     private SurfaceView surfaceView;
-    private SurfaceHolder surfaceHolder;
-
     private ImageView pictureImageView;
 
-    private Camera camera;
-    private Camera.Parameters parameters;
-    private CameraInfo currCameraInfo;
-    private MyOrientationEventListener orientationEventListener;
-
-    private int previewW;
-    private int previewH;
-    private int displayOrientation;
+    private CameraContext cameraContext;
     private int currentCameraIdType = Camera.CameraInfo.CAMERA_FACING_BACK;
-    private int rotation;
 
-    private Camera.AutoFocusMoveCallback cafCallback = new Camera.AutoFocusMoveCallback() {
-        @Override
-        public void onAutoFocusMoving(boolean start, Camera camera) {
-            Log.e(TAG, "onAutoFocusMoving: start " + start);
-            Toast.makeText(MainActivity.this,
-                    "focus " + (start ? "start" : "end"), Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    private Camera.AutoFocusCallback afCallback = new Camera.AutoFocusCallback() {
-        @Override
-        public void onAutoFocus(boolean success, Camera camera) {
-            Log.d(TAG, "onAutoFocus: success = " + success);
-            Toast.makeText(MainActivity.this,
-                    "focus " + (success ? "success" : "fail"), Toast.LENGTH_SHORT).show();
-        }
-    };
-
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -85,74 +53,72 @@ public class MainActivity extends AppCompatActivity
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[] {Manifest.permission.CAMERA}, 1000);
         }
-
-        orientationEventListener = new MyOrientationEventListener(this);
+        cameraContext = new CameraContext(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        orientationEventListener.enable();
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                cameraContext.resume();
+            }
+        });
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        closeCamera();
-        orientationEventListener.disable();
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                cameraContext.closeCamera();
+                cameraContext.pause();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        executor.shutdown();
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        surfaceHolder = holder;
-        openCamera(currentCameraIdType);
+        final SurfaceHolder surfaceHolder = holder;
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                cameraContext.configSurfaceHolder(surfaceHolder);
+                cameraContext.openCamera(currentCameraIdType);
+            }
+        });
     }
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        previewW = width;
-        previewH = height;
+        final int previewW = width;
+        final int previewH = height;
         Log.d(TAG, "surfaceChanged: format = " + format + ",w = " + width + ", h = " + height);
 
         surfaceView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                int x = (int) event.getX();
-                int y = (int) event.getY();
-                boolean isMirror = currentCameraIdType == Camera.CameraInfo.CAMERA_FACING_FRONT;
+                final int x = (int) event.getX();
+                final int y = (int) event.getY();
+                final boolean isMirror = currentCameraIdType == Camera.CameraInfo.CAMERA_FACING_FRONT;
 
-                // meter
-                int maxNumMeteringAreas = parameters.getMaxNumMeteringAreas();
-                if (maxNumMeteringAreas > 0) {
-                    Rect tapRect = CameraUtils.calculateTapArea(
-                            200, 200, x, y,
-                            previewW, previewH, displayOrientation, 1.0f, isMirror);
-                    List<Camera.Area> meteringAreas = new ArrayList<>();
-                    Camera.Area area = new Camera.Area(tapRect, 1000);
-                    meteringAreas.add(area);
-                    parameters.setMeteringAreas(meteringAreas);
-                    camera.setParameters(parameters);
-                }
-
-                // focus
-                if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
-                    camera.cancelAutoFocus();
-                    parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-                    int maxNumFocusAreas = parameters.getMaxNumFocusAreas();
-                    if (maxNumFocusAreas > 0) {
-                        List<Camera.Area> areas = new ArrayList<>();
-                        Rect tapRect = CameraUtils.calculateTapArea(
-                                200, 200, x, y,
-                                previewW, previewH, displayOrientation,1.5f, isMirror);
-                        Camera.Area area = new Camera.Area(tapRect, 1000);
-                        areas.add(area);
-                        parameters.setFocusAreas(areas);
+                executor.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (cameraContext != null) {
+                            cameraContext.onTouchAF(x, y, 200, 200, previewW, previewH, isMirror);
+                        }
                     }
-                    camera.setParameters(parameters);
-                    camera.autoFocus(afCallback);
-                    camera.setAutoFocusMoveCallback(null);
-                }
+                });
 
                 return false;
             }
@@ -173,119 +139,35 @@ public class MainActivity extends AppCompatActivity
                 currentCameraIdType = CameraInfo.CAMERA_FACING_BACK;
             }
 
-            closeCamera();
-            openCamera(currentCameraIdType);
-        } else if (v.getId() == R.id.capture_btn) {
-            capture();
-        } else if (v.getId() == R.id.picture_image_view) {
-            pictureImageView.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    private void closeCamera() {
-        if (camera != null) {
-            camera.stopPreview();
-            camera.release();
-            camera = null;
-        }
-    }
-
-    private void openCamera(int type) {
-        currCameraInfo = new CameraInfo(type);
-        int cameraId = currCameraInfo.getCameraId();
-        camera = Camera.open(cameraId);
-        parameters = camera.getParameters();
-        List<Camera.Size> previewSizeList = parameters.getSupportedPreviewSizes();
-        for (Camera.Size size: previewSizeList) {
-            Log.d(TAG, "preview size w " + size.width + ", h = " + size.height);
-        }
-
-        List<Camera.Size> pictureSizeList = parameters.getSupportedPictureSizes();
-        for (Camera.Size size: pictureSizeList) {
-            Log.d(TAG, "picture size w " + size.width + ", h = " + size.height);
-        }
-        parameters.setPreviewSize(1920, 1080);
-        parameters.setPictureSize(800, 600);
-
-        if (parameters.getSupportedFocusModes().contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
-            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-            camera.setAutoFocusMoveCallback(cafCallback);
-        }
-        camera.setParameters(parameters);
-
-        displayOrientation = 0;
-        try {
-            camera.setPreviewDisplay(surfaceHolder);
-            displayOrientation = CameraUtils.getCameraDisplayOrientation(this, cameraId);
-            camera.setDisplayOrientation(displayOrientation);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        camera.startPreview();
-        Log.d(TAG, "openCamera: " + currCameraInfo);
-    }
-
-    private void capture() {
-        if (camera != null) {
-            parameters.setRotation(rotation);
-            camera.setParameters(parameters);
-            camera.takePicture(new Camera.ShutterCallback() {
-                /**
-                 * Called as near as possible to the moment when a photo is captured
-                 * from the sensor.  This is a good opportunity to play a shutter sound
-                 * or give other feedback of camera operation.  This may be some time
-                 * after the photo was triggered, but some time before the actual data
-                 * is available.
-                 */
+            executor.submit(new Runnable() {
                 @Override
-                public void onShutter() {
-                    Log.d(TAG, "onShutter: ");
-                }
-            }, new Camera.PictureCallback() {
-                @Override
-                public void onPictureTaken(byte[] data, Camera camera) {
-                    // raw data
-                    Log.d(TAG, "onPictureTaken: raw");
-                }
-            }, new Camera.PictureCallback() {
-                @Override
-                public void onPictureTaken(byte[] data, Camera camera) {
-                    camera.startPreview();
-
-                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                    if (bitmap == null) {
-                        return;
+                public void run() {
+                    if (cameraContext != null) {
+                        cameraContext.switchCamera(currentCameraIdType);
                     }
-
-                    Log.d(TAG, "onPictureTaken");
-                    pictureImageView.setImageBitmap(bitmap);
-                    pictureImageView.setVisibility(View.VISIBLE);
                 }
             });
-        }
-    }
-
-    private class MyOrientationEventListener extends OrientationEventListener {
-
-        public MyOrientationEventListener(Context context) {
-            super(context);
-        }
-
-        @Override
-        public void onOrientationChanged(int orientation) {
-            if (orientation == ORIENTATION_UNKNOWN) return;
-            orientation = (orientation + 45) / 90 * 90;
-            int degrees;
-            if (currCameraInfo.getFacing() == CameraInfo.CAMERA_FACING_FRONT) {
-                degrees = (currCameraInfo.getPictureNeedRotateOrientation() - orientation + 360) % 360;
-            } else { // back-facing camera
-                degrees = (currCameraInfo.getPictureNeedRotateOrientation() + orientation) % 360;
-            }
-
-            if (rotation != degrees) {
-                rotation = degrees;
-                Log.d(TAG, "onOrientationChanged: rotation = " + rotation + ", orientation = " + orientation);
-            }
+        } else if (v.getId() == R.id.capture_btn) {
+            executor.submit(new Runnable() {
+                @Override
+                public void run() {
+                    if (cameraContext != null) {
+                        cameraContext.capture(new CameraContext.PictureCallback() {
+                            @Override
+                            public void onPictureTaken(byte[] data, Camera camera) {
+                                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                if (bitmap == null) {
+                                    return;
+                                }
+                                pictureImageView.setImageBitmap(bitmap);
+                                pictureImageView.setVisibility(View.VISIBLE);
+                            }
+                        });
+                    }
+                }
+            });
+        } else if (v.getId() == R.id.picture_image_view) {
+            pictureImageView.setVisibility(View.INVISIBLE);
         }
     }
 }
